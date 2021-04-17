5cf84e923bb25933d2fa6b6360d90fce2bac78af
ArielZ <38906564+Zzixin@users.noreply.github.com>
2021-04-17 21:50:07 +0800

    Update and rename SmallCalendar to SmallCalendar.java

--- codes/SmallCalendar.java
+++ codes/SmallCalendar.java
@@ -12,7 +12,8 @@ public class SmallCalendar {
         System.out.println("Mon Tue Wed THU fri sat sun");
         for (int i=1; i<week; i++)
             System.out.print("    "); //四空格(Mon )
-
+        
+        //outprint the calender
         while (date.getMonthValue() == month){
             System.out.printf("%3d",date.getDayOfMonth()); //每个日期占四空格，对齐
             if (date.getDayOfMonth()==today)
@@ -23,6 +24,7 @@ public class SmallCalendar {
             if (date.getDayOfWeek().getValue() == 1) //newline if it's Monday
                 System.out.println();
         }
+        
         if (date.getDayOfWeek().getValue() != 1)
             System.out.println();
     }
